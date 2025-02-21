7ea9a02cc469652d7993971a57ed1bf7fc0a046d
sidhantrajput1 <sidhant808310@gmail.com>
2025-02-21 09:45:42 +0530

    Modal Window

---
+++ 04-Model Window/Readme.md
@@ -0,0 +1,9 @@
+addEventListener
+    The addEventListener() method in JavaScript allows you to attach event handlers (or listeners) to HTML elements. This lets you execute code in response to specific events like clicks, key presses, or hover actions.
+
+element.addEventListener(event, function, useCapture);
+
+classList :-
+   The classList property in JavaScript is used to manage the classes of an HTML element. It provides a way to add, remove, toggle, or check classes without needing to modify the entire class attribute directly.
+
+keypress event is triggered when a key is pressed down and held on the keyboard. It is commonly used to handle input in text fields or forms
--- 04-Model Window/script.js
+++ 04-Model Window/script.js
@@ -1,3 +1,4 @@
+'use strict'
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
@@ -22,6 +23,11 @@ btnCloseModal.addEventListener('click', closeModal)
 overlay.addEventListener('click', closeModal)
 
 
+document.addEventListener('keydown', function(e) {
+    if(e.key === 'Escape') 
+        if(!modal.classList.contains('hidden')) closeModal();
+})
+
 /* 
 
 for(let i = 0; i< btnOpenModal.length; i++) {
@@ -39,6 +45,25 @@ btnCloseModal.addEventListener('click', function() {
 overlay.addEventListener('click', function() {
     modal.classList.add('hidden')
     overlay.classList.add("hidden")
+})
+
+
+document.addEventListener('keydown', function(e) {
+    // console.log("A key has Pressed")
+    // console.log(e.key)
+
+    if(e.key === 'Escape') {
+        modal.classList.add('hidden')
+        overlay.classList.add('hidden')
+    }
+    
+    // this code is also work same as above code 
+    if(e.key === 'Escape') closeModal();
+
+    // this is  also
+    if(e.key === 'Escape') {
+        if(!modal.classList.contains('hidden')) closeModal();
+    }
 })
 
 
