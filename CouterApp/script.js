c28a1326d9226fec9597fdd841ff232d81e2f401
sidhantrajput1 <sidhant808310@gmail.com>
2025-05-21 23:55:06 +0530

    Counter App

---
+++ CouterApp/script.js
@@ -0,0 +1,50 @@
+"use strict";
+
+const increaseBtn = document.querySelector(".increaseBtn");
+const decreaseBtn = document.querySelector(".decreaseBtn");
+const counter = document.querySelector("#counter");
+const resetBtn = document.querySelector(".resetBtn");
+
+// let count = 1;
+
+// increaseBtn.addEventListener('click', function(e) {
+//     count++;
+//     counter.textContent = count;
+// })
+
+// decreaseBtn.addEventListener('click', function(e) {
+//     if (count > 1) {
+//         count--;
+//         counter.textContent = count;
+//     }
+// })
+
+// resetBtn.addEventListener('click', function(e) {
+//     count = 1;
+//     counter.textContent = count;
+// })
+
+let count = localStorage.getItem("count")
+  ? parseInt(localStorage.getItem("count"))
+  : 1;
+counter.textContent = count;
+
+function updateCounter(newCount) {
+    count = newCount;
+    counter.textContent = count;
+    localStorage.setItem("count", count)
+}
+
+increaseBtn.addEventListener('click', function() {
+    updateCounter(count + 1)
+})
+
+decreaseBtn.addEventListener('click', function() {
+    if (count > 1) {
+        updateCounter(count - 1)
+    }
+})
+
+resetBtn.addEventListener('click', function() {
+    updateCounter(1)
+})
